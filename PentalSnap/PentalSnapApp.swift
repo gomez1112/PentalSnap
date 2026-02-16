83dbea5e53015b72d83391179336c39fc5eace8b
gomez1112 <54489633+gomez1112@users.noreply.github.com>
2026-02-16 14:09:07 -0500

    Initial Commit

---
+++ PentalSnap/PentalSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  PentalSnapApp.swift
+//  PentalSnap
+//
+//  Created by Gerard Gomez on 2/16/26.
+//
+
+import SwiftUI
+
+@main
+struct PentalSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
